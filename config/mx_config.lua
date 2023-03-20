d1b96cee568f0b562c19d67b174ff3e7f620a617
Matěj Bednář <bednar.matej2@seznam.cz>
2023-03-21 00:08:53 +0100

    Update mx_config.lua

--- config/mx_config.lua
+++ config/mx_config.lua
@@ -1,9 +1,9 @@
 Config = {}
 
-Config.MXlink = 'https://discord.com/api/webhooks/1073708343073439895/obv1B6w79hKA8dq2-SCvCRBy9a1IZDypbRLXnmPqO8VydBoLrnlnQSFvJh2DkMR6XbHG' -- Default Webhook Link
+Config.MXlink = 'https://discord.com/api/webhooks/YOUR-WEBHOOK-LINK' -- Default Webhook Link
 
 Config.loginLog = true -- Log when players join and leave?
-Config.loginLogLink = 'https://discord.com/api/webhooks/1073708343073439895/obv1B6w79hKA8dq2-SCvCRBy9a1IZDypbRLXnmPqO8VydBoLrnlnQSFvJh2DkMR6XbHG' -- WebHook Link to log logis. (Only works when Confi.loginLog = true).
+Config.loginLogLink = 'https://discord.com/api/webhooks/YOUR-WEBHOOK-LINK' -- WebHook Link to log logis. (Only works when Confi.loginLog = true).
 
 
 Config.MXName = 'MX-Logs' -- Name of the WebHook
